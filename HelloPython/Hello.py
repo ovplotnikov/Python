b895d0e2ec2829f52ddb52743aad0767c4f99515
Oleg <o.v.plotnikov@gmail.com>
2022-09-23 15:46:41 +0600

    seminar_1

---
+++ HelloPython/Hello.py
@@ -0,0 +1,26 @@
+a = 123
+c = 1.23
+d = True
+b = 'qwerty'
+print(a)
+print(b)
+print(c)
+print(d)
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+print(a, '-', b, '-', c, '-', d)
+print(f'{a} - {b} - {c} - {d}')
+print('{} - {} - {} - {}'.format(a, b, c, d))
+
+list = [1, '1', 1.1, 'qwerty', True]
+print (list)
+
+print ('Введите а')
+a = float(input())
+print ('Введите b')
+b = float(input())
+print ('a + b = ', a + b)
